cadb41c971ffe9aafe3645069a04cd9ae14c587b
Dhakshinesh2001 <dhakshinesh5771@gmail.com>
2025-05-14 23:05:39 +0530

    initialised the servers

---
+++ apps/web-socket-server/src/index.ts
@@ -0,0 +1,12 @@
+import WebSocket from 'ws'; 
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log('received: %s', message);
+        ws.send('something');
+    });
+
+   
+});
